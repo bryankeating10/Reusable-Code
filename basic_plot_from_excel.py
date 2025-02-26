fbdcde34135caf80d35b0ab6939bd3fc2b290df7
Bryan Keating <158529559+bryankeating10@users.noreply.github.com>
2025-02-26 15:02:55 -0500

    Update basic_plot_from_excel.py

--- basic_plot_from_excel.py
+++ basic_plot_from_excel.py
@@ -8,31 +8,34 @@ import matplotlib.pyplot as plt
 
 # Customization
 file_path = "path/file.xlsx"
-plot_type = input('Would you like separate plots (P) or a superimposed plot (I): ')
 
 # Plots the data
 data = pd.read_excel(file_path)
 x_val = data.columns[0]
 
-if plot_type == "I":
-	plt.figure()
-	for col in data.columns[1:]:
-		plt.plot(data[x_val],data[col], marker='o', label = col)
-	plt.title(input("Title: "))
-	plt.xlabel(x_val)
-	plt.ylabel(input("Y Label: "))
-	plt.legend()  # Automatically adds a legend for each line
-	plt.grid(True)
-	plt.show()
-
-elif plot_type == "P":
-	for col in data.columns[1:]:
-		plt.figure(col)
-		plt.plot(data[x_val],data[col], marker='o')
-		plt.title(input(f"Title for {col}: "))
+while True:
+	plot_type = input('Would you like separate plots (P) or a superimposed plot (I): ')
+	if plot_type == "I":
+		plt.figure()
+		for col in data.columns[1:]:
+			plt.plot(data[x_val],data[col], marker='o', label = col)
+		plt.title(input("Title: "))
 		plt.xlabel(x_val)
-		plt.ylabel(input(f"Y Label for {col}: "))
+		plt.ylabel(input("Y Label: "))
+		plt.legend()  # Automatically adds a legend for each line
 		plt.grid(True)
-	plt.show()
-else:
-	print('Not a valid plot type. Use "I" or "P"')
+		plt.show()
+		break
+	
+	elif plot_type == "P":
+		for col in data.columns[1:]:
+			plt.figure(col)
+			plt.plot(data[x_val],data[col], marker='o')
+			plt.title(input(f"Title for {col}: "))
+			plt.xlabel(x_val)
+			plt.ylabel(input(f"Y Label for {col}: "))
+			plt.grid(True)
+		plt.show()
+		break
+	else:
+		print('Not a valid plot type. Use "I" or "P"')
